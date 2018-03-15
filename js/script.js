0f980051e512f5b494a621a627e07d7b31226ee4
fatxlam <fatxlam@yandex.ru>
2018-03-16 00:32:29 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*menu.onclic = function test(){
+	var x = document.getElementById("nTopnav");
+	if (x.className === topnav){
+		x.className += "responsive";
+	} else {
+		x.className = "topnav";
+	}
+}*/
